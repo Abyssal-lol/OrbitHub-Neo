305e810cb7284f8de752f2c1ccbde28bd34e74db
Abyssal-lol <abyssallol090@gmail.com>
2026-05-29 20:49:00 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,9 +1,11 @@
 if getgenv().Config == "Chest" then
   -- chest here
+  game.Players.LocalPlayer:Kick("This part is still in development")
 elseif getgenv().Config == "Hub" then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/OrbitHub-Neo/refs/heads/main/Loader/GameLoader.lua"))()
 elseif getgenv().Config == "Hop" then
   -- hop
+  game.Players.LocalPlayer:Kick("This part is still in development")
 else
   print("--[[ No Config ]]--")
   -- Game Loader
