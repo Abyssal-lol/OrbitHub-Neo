09a579350a3dfedc270ea30538f6a120d9e59f63
Abyssal-lol <abyssallol090@gmail.com>
2026-05-30 14:42:00 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,7 +1,11 @@
+function customSfx()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/OrbitHub-Neo/refs/heads/main/Sounds/Main.lua"))()
+end
 if getgenv().Config == "Chest" then
   -- chest here
   game.Players.LocalPlayer:Kick("This part is still in development")
 elseif getgenv().Config == "Hub" then
+  customSfx()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/OrbitHub-Neo/refs/heads/main/Loader/GameLoader.lua"))()
 elseif getgenv().Config == "Hop" then
   -- hop
@@ -9,5 +13,6 @@ elseif getgenv().Config == "Hop" then
 else
   print("--[[ No Config ]]--")
   -- Game Loader
+  customSfx()
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/OrbitHub-Neo/refs/heads/main/Loader/GameLoader.lua"))()
 end
