8e751ee458f18da22980757c975ae3f0953fbe13
Abyssal-lol <abyssallol090@gmail.com>
2026-05-30 14:40:43 +0800

    Create Main.lua

---
+++ Sounds/Main.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 7449423635 or game.PlaceId == 100117331123089 the
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/Abyssal-lol/OrbitHub-Neo/refs/heads/main/Sounds/BloxFruit/Config.lua"))()
+else
+  print("no")
+end
